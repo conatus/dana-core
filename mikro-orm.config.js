8a180ff3d6b51aeba98b62fb76359e477399ded8
Chris Devereux <chrisdevereux@users.noreply.github.com>
2022-03-30 12:47:07 +0000

    Fix invalid mac/windows path for migration snapshot

--- mikro-orm.config.js
+++ mikro-orm.config.js
@@ -1,11 +1,14 @@
 /**
- * @type {import('vite').Options}
+ * @type {import('@mikro-orm/core').Options<import('@mikro-orm/sqlite').SqliteDriver>}
  */
 module.exports = {
   entities: ['src/app/**/*.entity.ts'],
   migrations: {
     path: 'src/app/migrations'
   },
-  dbName: ':memory:',
+  dbName: 'tmp/migrations.sqlite',
+  driverOptions: {
+    dbName: ':memory:'
+  },
   type: 'sqlite'
 };
